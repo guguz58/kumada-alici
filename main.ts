c477f2fee1997ed0c223df017b5311e2670678df
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2024-05-24 10:25:43 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,18 @@ radio.setGroup(3)
 basic.forever(function () {
     if (WSJoyStick.Listen_Dir(DIR.U)) {
         radio.sendValue("name", 1)
+        basic.showArrow(ArrowNames.North)
     }
     if (WSJoyStick.Listen_Dir(DIR.D)) {
         radio.sendValue("name", 2)
+        basic.showArrow(ArrowNames.South)
     }
     if (WSJoyStick.Listen_Dir(DIR.L)) {
         radio.sendValue("name", 3)
+        basic.showArrow(ArrowNames.West)
     }
     if (WSJoyStick.Listen_Dir(DIR.R)) {
         radio.sendValue("name", 4)
+        basic.showArrow(ArrowNames.East)
     }
 })
